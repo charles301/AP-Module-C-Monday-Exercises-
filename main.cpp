16d308dc1d9f17d323cbff7695b0e7dbfdf100c9
Charles Wilson <charles.wilson@ada.ac.uk>
2021-01-11 12:38:32 +0000

    Copy from another repo

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
 
 int main() {
-  std::cout << "Hello World!\n";
+  std::string quote;
+  std::string author;
+
+  std::cout << "Please input a quote!\n";
+
+  getline(std::cin, quote);
+
+  std::cout << "And the author who said the quote?  \n";
+
+  getline(std::cin, author);
+
+  std::cout << "A wise man once said: \"" << quote <<"\"\n"
+  "- "<< author << "\n";
 }
+
